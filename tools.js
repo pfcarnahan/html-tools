bf8b849c65763b90209daf19c2eccfdd5c891862
pfcarnahan <163607933+pfcarnahan@users.noreply.github.com>
2024-03-16 13:55:15 -0500

    Update tools.js

--- tools.js
+++ tools.js
@@ -1,4 +1,15 @@
-var switchPage = function(number) {
-	var all = document.getElementsByTagName("page")
-	console.log(all)
+var switchPage = function(page) {
+	var all = document.querySelectorAll("*")
+	for(var i = 0; i < all.length; i++) {
+		var attrs = all[i].attributes
+		for(var j = 0; j < attrs.length; j++) {
+			if(attrs.item(j).name === "page") {
+				if(attrs.item(j).value === toPage) {
+					all[i].style.display = "block"
+				} else {
+					all[i].style.display = "none"
+				}
+			}
+		}
+	}
 }
