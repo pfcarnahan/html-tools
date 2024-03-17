b6fb5d8126bb75e0e78f0aacf2e7f21691748d1e
pfcarnahan <163607933+pfcarnahan@users.noreply.github.com>
2024-03-17 13:23:00 -0500

    Update tools.js
    Added bindButton and bindButtons

--- tools.js
+++ tools.js
@@ -12,4 +12,21 @@ var switchPage = function(page) {
 			}
 		}
 	}
-}
+} // Switches the current page to those elements that match page = 'page'
+
+var bindButton = function(id, page) {
+	document.getElementById(id).addEventListener("click", () => switchPage(page))
+} // Binds button with id 'id' to switch page to 'page'
+
+var bindButtons = function(className) {
+	var buttons = document.querySelectorAll(`button.${className}`)
+	for(var i = 0; i < buttons.length; i++) {
+		var attrs = buttons[i].attributes
+		for(var j = 0; j < attrs[i].length; j++) {
+			if(attrs.item(j).name === "to") {
+				bindButton(buttons[i].id, attrs.item(j).value)
+				break;
+			}
+		}
+	}
+} // Binds all buttons of the given 'className' to their specified to = 'page id'
